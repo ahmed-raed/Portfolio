324503786459dbeccd58e2d8c40f1e5d791df225
ahmed-raed <ahmedraed.dev@gmail.com>
2024-05-21 01:05:26 +0300

    fixed: resume link

--- app/about/page.js
+++ app/about/page.js
@@ -28,7 +28,7 @@ export default function About() {
               priority
             />
             <Link
-              href="https://drive.google.com/file/d/1DpRiyLFSCqY-HNFUZ-rGqAg5YLyUivq6"
+              href="https://drive.google.com/file/d/1jFVSbepp8IseYfHff7_gwMrm5ffIb-MR/view?usp=sharing"
               target="_blank"
               className={styles.bluring}
             >
